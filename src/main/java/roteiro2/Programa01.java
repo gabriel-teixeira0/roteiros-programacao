0257816ffa35a6d9ee72a6d566cb984b4d066c42
Gabriel Teixeira Andrade <gabrielteixa800@gmail.com>
2026-06-28 01:56:31 -0300

    Adicionar pasta principal

---
+++ src/main/java/roteiro2/Programa01.java
@@ -0,0 +1,13 @@
+package roteiro2;
+
+/**
+ *
+ * @Gabriel Teixeira
+ * Github: gabriel-teixeira0
+ */
+public class Programa01 {
+
+    public static void main(String[] args) {
+        System.out.println("Primeiro Programa");
+    }
+}
